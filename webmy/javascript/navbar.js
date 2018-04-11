4ee447500a265ba8dc76ed9683ab04d51d08bfd2
KevinMathew <mathewtkevin@gmail.com>
2018-04-11 18:48:08 +0530

    Done designing front page

--- webmy/javascript/navbar.js
+++ webmy/javascript/navbar.js
@@ -8,15 +8,13 @@ function myFunction() {
     }
 }
 
-
-
+var navbar = document.getElementById("myTopnav");
+var sticky = navbar.offsetTop;
 window.onscroll = function() {myFunction2()};
 
 // Get the navbar
-var navbar = document.getElementById("myTopnav");
 
 // Get the offset position of the navbar
-var sticky = navbar.offsetTop;
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction2() {
